4d9e9d3c6b3257c2fd5d2654a350511b16a4317c
Camille Barbit <barbitcamille@gmail.com>
2022-07-14 19:44:01 +0200

    Updated version

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:43:25 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/29 18:43:06 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/07/14 19:33:35 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int	ft_strncmp(char const *s1, char const *s2, size_t n)
 	i = 0;
 	s1bis = (unsigned char *)s1;
 	s2bis = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
 	while (s1bis[i] && s2bis[i] && s1bis[i] == s2bis[i] && i < n - 1)
 		i++;
 	return (s1bis[i] - s2bis[i]);
--- parsing/validity_argv1.c
+++ parsing/validity_argv1.c
@@ -6,11 +6,34 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 13:11:43 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/07/14 19:12:29 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/07/14 19:42:43 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
+
+/**
+* @param str 
+    str is my argv1 (the file with the extension .cub)
+* * The function counts the number of dots it encounters in the string
+* ! There cannot be more that one dot! If count != 1, then it is wrong
+**/
+
+static int	count_dots(const char *str)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '.')
+			count++;
+		i++;
+	}
+	return (count);
+}
 
 /**
 * @param str
@@ -21,8 +44,16 @@
 
 int	check_validity_argv1(const char *str)
 {
-	if (ft_strlen(str) < 4)
+	int	len;
+	int	start;
+
+	if (count_dots(str) != 1)
 		return (EXIT_FAILURE);
-	
+	len = ft_strlen(str);
+	start = len - 4;
+	if (len < 4)
+		return (EXIT_FAILURE);
+	if (ft_strncmp(str + start, ".cub", 4) != 0)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
