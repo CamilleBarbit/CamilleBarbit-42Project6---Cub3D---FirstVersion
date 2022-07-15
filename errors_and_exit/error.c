0b52c631aded0e1279369248ecb219330a3a1e51
CamilleBarbit <85878035+CamilleBarbit@users.noreply.github.com>
2022-07-15 19:48:08 +0200

    Add files via upload

--- errors_and_exit/error.c
+++ errors_and_exit/error.c
@@ -6,20 +6,20 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 19:29:39 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/07/14 19:20:45 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/07/15 19:42:20 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
 /**
-* @param
-	str is the error message to print
-* *Function printing error message
+*	@param
+		str is the error message to print
+*	*Function printing error message
 **/
 
 void	ft_error(const char *str)
 {
-	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd(str, 2);
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
 }
